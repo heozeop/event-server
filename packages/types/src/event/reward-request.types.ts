7fcf79b3ae19cb47b68f2f46ca375454165c9b4e
crispy <dltmdcks702@cau.ac.kr>
2025-05-21 00:06:07 +0900

    feat(event): add condition property to RewardRequestEntity for enhanced reward request management

--- packages/types/src/event/reward-request.types.ts
+++ packages/types/src/event/reward-request.types.ts
@@ -1,7 +1,7 @@
 import { RewardRequestStatus } from "@libs/enums";
 import { ObjectId } from "@mikro-orm/mongodb";
-import { EventEntity } from "./event.types";
 import { CustomBaseEntity } from "../base.types";
+import { EventEntity } from "./event.types";
 
 /**
  * Interface representing the RewardRequest entity
@@ -10,4 +10,5 @@ export interface RewardRequestEntity extends CustomBaseEntity {
   userId: ObjectId;
   event: EventEntity;
   status: RewardRequestStatus;
+  condition: Record<string, any>;
 }
